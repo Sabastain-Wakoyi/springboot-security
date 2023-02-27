26e8051d38a8ef7343c80f9a4b2c5b65851bc949
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2023-02-27 16:59:47 -0600

    writing security test

--- src/main/java/com/snva/securityboot/Config/SecurityConfig.java
+++ src/main/java/com/snva/securityboot/Config/SecurityConfig.java
@@ -1,14 +1,25 @@
 package com.snva.securityboot.Config;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 public class SecurityConfig {
 
     protected void configure(HttpSecurity http) throws  Exception{
         http
+
+
                 .formLogin()
                 .and()
                 .authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/admin").hasRole("ADMIN")
+                .antMatchers(HttpMethod.GET,"/premium").hasRole("PREMIUM")
+                .antMatchers(HttpMethod.GET,"/user").hasRole("USER")
+                .and()
+                .csrf().disable();
+
+
+
 
 
     }
